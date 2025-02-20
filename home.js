eccb3abc73dcbe9c4f961e52d657b8450e0cb812
vinny caesar <vinnycaesar89@example.com>
2025-02-20 00:50:04 -0800

    changed footer style

--- home.js
+++ home.js
@@ -10,3 +10,14 @@ function showSlides() {
     slides[slideIndex - 1].style.display = 'block';
     setTimeout(showSlides, 4000); // Change slide every 4 seconds
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const imageContainer = document.querySelector('.image-container');
+observer.observe(imageContainer);
